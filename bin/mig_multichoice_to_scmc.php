de158db215b6c235b52f1fd6e7483c0a2e45c27d
ahourani <amr.hourani@id.ethz.ch>
2016-04-12 14:40:17 +0200

    suffix the newly copied multichoice

--- bin/mig_multichoice_to_scmc.php
+++ bin/mig_multichoice_to_scmc.php
@@ -183,7 +183,7 @@ foreach ($questions as $question) {
     // Create a new scmc question in the same category.
     unset($question->id);
     $question->qtype = 'scmc';
-    $question->name = $question->name . ' (NEW55 scmc)';
+    $question->name = $question->name . ' (scmc)';
     $question->timecreated = time();
     $question->timemodified = time();
     $question->modifiedby = $USER->id;
