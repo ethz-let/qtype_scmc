4d13547b25a4b66bf9bc907d98b81fa3789dc2e2
ahourani <amr.hourani@id.ethz.ch>
2016-06-08 12:27:38 +0200

    releasing functionality

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_scmc';
-$plugin->version = 2016060600;
+$plugin->version = 2016060800;
 $plugin->requires = 2013111904; // Moodle >=2.6.4.
 
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.5 for Moodle 2.6+';
+$plugin->release = '1.6 for Moodle 2.6+';
