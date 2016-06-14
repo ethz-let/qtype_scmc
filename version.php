5afe3dc03fe54f667778b98b1a029360d4704349
ahourani <amr.hourani@id.ethz.ch>
2016-06-14 15:10:59 +0200

    pump the version

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_scmc';
-$plugin->version = 2016061400;
+$plugin->version = 2016061500;
 $plugin->requires = 2013111904; // Moodle >=2.6.4.
 
 $plugin->maturity = MATURITY_STABLE;
