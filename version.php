bc67ea52d2246c6397251e137a60745cfe03ea6e
ahourani <amr.hourani@id.ethz.ch>
2016-06-17 10:55:01 +0200

    supporting moodle 3.1+ tags

--- version.php
+++ version.php
@@ -26,4 +26,4 @@ $plugin->version = 2016061700;
 $plugin->requires = 2013111904; // Moodle >=2.6.4.
 
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.7 for Moodle 2.6+';
+$plugin->release = '2.8 for Moodle 2.6+';
